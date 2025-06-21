8d044bd617aac1eb04919f1c4dc477e153b099ab
reshamsu <resham.iox@gmail.com>
2025-06-22 00:26:39 +0530

    ChatWidget Update

--- src/api/ChatBot.jsx
+++ src/api/ChatBot.jsx
@@ -3,11 +3,9 @@ import React, { useEffect } from 'react';
 const ChatBot = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://widgets.leadconnectorhq.com/loader.js';
-    script.setAttribute(
-      'data-resources-url',
-      'https://widgets.leadconnectorhq.com/chat-widget/YOUR_ACCOUNT_ID.js'
-    );
+    script.src = 'https://beta.leadconnectorhq.com/loader.js';
+    script.setAttribute('data-resources-url', 'https://beta.leadconnectorhq.com/chat-widget/loader.js');
+    script.setAttribute('data-widget-id', '6856b070d9a1b4e219c4a999');
     script.async = true;
     document.body.appendChild(script);
 
@@ -16,7 +14,7 @@ const ChatBot = () => {
     };
   }, []);
 
-  return null; // No visual elements needed
+  return null; // This component does not render any JSX
 };
 
 export default ChatBot;
